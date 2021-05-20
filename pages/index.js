910271504c827168cb0c886f92a2c20aeeb25be7
Lee Ingram <lee@soylentcola.com>
2021-05-19 17:43:14 -0700

    Adding fallback URLs for IP lookup

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Router from 'next/router';
 import publicIp from "public-ip";
 
 export const getClientIp = async () => await publicIp.v4({
-  fallbackUrls: [ "https://ifconfig.co/ip" ]
+  fallbackUrls: [ "https://ifconfig.co/ip","http://icanhazip.com/","https://ifconfig.io/ip" ]
 });
 
 function Form(args) {
