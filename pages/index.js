fd17e40ab522bbf8613e0b346fea342c7d41994e
Lee Ingram <lee@soylentcola.com>
2021-09-12 11:33:57 -0700

    Formatting again

--- pages/index.js
+++ pages/index.js
@@ -151,8 +151,7 @@ export default function Home(props) {
         <h1 className="title">
           <Link href="https://bscscan.com/token/0xB44cf912E9D0341e92f64f4a0642393B7f3526C4"><a>Rug</a></Link> and Tug!
         </h1>
-        <p className="description">100,000 PULL on successful transaction!
-        <small>You are responsible for covering the gas fee. </small>
+        <p className="description">100,000 PULL just for covering the gas fee! Connect your wallet to get started!
         </p>
         <Account triedToEagerConnect={triedToEagerConnect} />
         <ETHBalance />
